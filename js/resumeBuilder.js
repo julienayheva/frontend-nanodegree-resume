fb2a14d7098deb61961886c2e78d96ca0f89def3
julienayheva <julienayheva@gmail.com>
2016-06-16 13:08:13 +0200

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formattedName;
+ var formattedRole;
+ 
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
+var formattedRole = HTMLheaderName.replace("%data%", role);
+$("#header").prepend(formattedRole);
